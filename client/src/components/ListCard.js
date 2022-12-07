a9246c13e43312db2ddbe2cf9da93442ae04538e
Weikang Yang <96400625+HarryYang1008@users.noreply.github.com>
2022-12-06 22:11:33 -0500

    fix part of UI aad new bar/ fix listcard

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -1,144 +1,211 @@
-import { useContext, useState } from 'react'
-import { GlobalStoreContext } from '../store'
-import Box from '@mui/material/Box';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import IconButton from '@mui/material/IconButton';
-import ListItem from '@mui/material/ListItem';
-import TextField from '@mui/material/TextField';
+import { useContext, useState } from "react";
+import { GlobalStoreContext } from "../store";
+import Box from "@mui/material/Box";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import IconButton from "@mui/material/IconButton";
+import ListItem from "@mui/material/ListItem";
+import TextField from "@mui/material/TextField";
+import ThumbUpIcon from "@mui/icons-material/ThumbUpAltOutlined";
+import ThumbDownIcon from "@mui/icons-material/ThumbDownOutlined";
+import DoubleDownArrowIcon from "@mui/icons-material/KeyboardDoubleArrowDownOutlined";
 
 /*
     This is a card in our list of top 5 lists. It lets select
+
+    
+        
+          
+    
+
+        
+    
+    @@ -17,14 +20,14 @@ import TextField from '@mui/material/TextField';
+  
     a list for editing and it has controls for changing its 
     name or deleting it.
     
     @author McKilla Gorilla
 */
 function ListCard(props) {
-    const { idNamePair, selected } = props;
-    const { store } = useContext(GlobalStoreContext);
-    const [ editActive, setEditActive ] = useState(false);
-    const [ text, setText ] = useState(idNamePair.name);
+    const {idNamePair,selected} = props;
+    const {store} = useContext(GlobalStoreContext);
+    const [editActive,setEditActive] = useState(false);
+    const [text,setText] = useState(idNamePair.name);
 
-    function handleLoadList(event, id) {
+    function handleLoadList(event,id) {
         console.log("handleLoadList for " + id);
-        if (!event.target.disabled) {
+        if(!event.target.disabled) {
             let _id = event.target.id;
-            if (_id.indexOf('list-card-text-') >= 0)
+            if(_id.indexOf("list-card-text-") >= 0)
                 _id = ("" + _id).substring("list-card-text-".length);
 
             console.log("load " + event.target.id);
 
-            // CHANGE THE CURRENT LIST
-            store.setCurrentList(id);
+    
+          
+            
+    
+
+          
+          
+            
+    
+
+          
+    
+            function ListCard(props) {
+  
+                // CHANGE THE CURRENT LIST
+                store.setCurrentList(id);
+            }
         }
-    }
-
-    async function handleDeleteList(event, id) {
-        event.stopPropagation();
-        let _id = event.target.id;
-        _id = ("" + _id).substring("delete-list-".length);
-        store.markListForDeletion(id);
-    }
-
-    function handleToggleEdit(event) {
-        event.stopPropagation();
-        toggleEdit();
-    }
-
-    function toggleEdit() {
-        let newActive = !editActive;
-        if (newActive) {
-            store.setIsListNameEditActive();
+        async function handleDeleteList(event,id) {
+            event.stopPropagation();
+            let _id = event.target.id;
+            _id = ("" + _id).substring("delete-list-".length);
+            store.markListForDeletion(id);
         }
-        setEditActive(newActive);
-    }
-
-    function handleKeyPress(event) {
-        if (event.code === "Enter") {
-            // let id = event.target.id.substring("list-".length);        
-            handleBlur();
+        function handleToggleEdit(event) {
+            event.stopPropagation();
+            toggleEdit();
         }
-    }
+        function toggleEdit() {
+            let newActive = !editActive;
+            if(newActive) {
+                store.setIsListNameEditActive();
+            }
+            setEditActive(newActive);
+        }
 
-    // handle blur->
-    function handleBlur() {
-        store.changeListName(idNamePair._id, text);
-        toggleEdit();
-    }
+        function handleKeyPress(event) {
+            if(event.code === "Enter") {
+                // let id = event.target.id.substring("list-".length);
+                handleBlur();
+            }
+        }
 
-    function handleUpdateText(event) {
-        setText(event.target.value );
-    }
+    
+        
+          
+    
 
-    // handle blur->
-    function handleBlur() {
-        // let id = event.target.id.substring("list-".length);
-        store.changeListName(idNamePair._id, text);
-        toggleEdit();
-    }
+        
+    
+        function ListCard(props) {
+  
+            // handle blur->
+            function handleBlur() {
+                store.changeListName(idNamePair._id,text);
+                toggleEdit();
+            }
 
-    function handleUpdateText(event) {
-        setText(event.target.value);
-    }
+            function handleUpdateText(event) {
+                setText(event.target.value);
+            }
 
-    let selectClass = "unselected-list-card";
-    if (selected) {
-        selectClass = "selected-list-card";
-    }
-    let cardStatus = false;
-    if (store.isListNameEditActive) {
-        cardStatus = true;
-    }
-    let cardElement =
-        <ListItem
-            id={idNamePair._id}
-            key={idNamePair._id}
-            sx={{ marginTop: '15px', display: 'flex', p: 1 }}
-            style={{ width: '100%', fontSize: '48pt' }}
-            button
-            onClick={(event) => {
-                handleLoadList(event, idNamePair._id)
-            }}
-        >
-            <Box sx={{ p: 1, flexGrow: 1 }}>{idNamePair.name}</Box>
-            <Box sx={{ p: 1 }}>
+    
+        
+          
+    
+
+        
+    
+            function ListCard(props) {
+  
+                let selectClass = "unselected-list-card";
+                if(selected) {
+                    selectClass = "selected-list-card";
+                }
+                let cardStatus = false;
+                if(store.isListNameEditActive) {
+                    cardStatus = true;
+                }
+                let cardElement = (
+                    <div
+                        id={idNamePair._id}
+                        key={idNamePair._id}
+                        style={{
+                            marginTop: "5px",
+                            display: "flex",
+                            width: "100%",
+                            border: "3px solid lightBlue",
+                            borderRadius: "20px",
+                            backgroundColor: "lightyellow",
+                        }}
+                        button
+                        onClick={event => {
+                            handleLoadList(event,idNamePair._id);
+                        }}
+                    >
+                        <div id='box1'>
+                            <div id='list-card-title' style={{flexGrow: 1}}>
+                                {idNamePair.name}
+                            </div>
+                            <div style={{marginLeft: "10px"}}>By: </div>
+                            <div style={{marginLeft: "10px",marginBottom: "7px"}}>
+                                published:
+                            </div>
+                        </div>
+                        <div id='box2' style={{position: 'relative'}}>
+                            <div style={{marginLeft: "10px"}}>
+                                <IconButton>
+                                    <ThumbUpIcon />
+                                </IconButton>
+                                10
+                                <IconButton>
+                                    <ThumbDownIcon />
+                                </IconButton>
+                                10
+                            </div>
+                            <div style={{position: 'absolute',right: '20px'}}>
+                                <DoubleDownArrowIcon />
+                            </div>
+                        </div>
+
+                        {/* <div >
                 <IconButton onClick={handleToggleEdit} aria-label='edit'>
-                    <EditIcon style={{fontSize:'48pt'}} />
+                    <EditIcon style={{ fontSize: "48pt" }} />
                 </IconButton>
-            </Box>
-            <Box sx={{ p: 1 }}>
-                <IconButton onClick={(event) => {
-                        handleDeleteList(event, idNamePair._id)
-                    }} aria-label='delete'>
-                    <DeleteIcon style={{fontSize:'48pt'}} />
+            </div>
+            <div >
+                <IconButton
+                    onClick={event => {
+                        handleDeleteList(event, idNamePair._id);
+                    }}
+                    aria-label='delete'
+                >
+                    <DeleteIcon style={{ fontSize: "48pt" }} />
                 </IconButton>
-            </Box>
-        </ListItem>
+            </div> */}
+                    </div>
+                );
 
-    if (editActive) {
-        cardElement =
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id={"list-" + idNamePair._id}
-                label="Playlist Name"
-                name="name"
-                autoComplete="Playlist Name"
-                className='list-card'
-                onKeyPress={handleKeyPress}
-                onBlur={handleBlur}
-                onChange={handleUpdateText}
-                defaultValue={idNamePair.name}
-                inputProps={{style: {fontSize: 48}}}
-                InputLabelProps={{style: {fontSize: 24}}}
-                autoFocus
-            />
+                if(editActive) {
+                    cardElement = (
+                        <TextField
+                            margin='normal'
+                            required
+                            fullWidth
+                            id={"list-" + idNamePair._id}
+                            label='Playlist Name'
+                            name='name'
+                            autoComplete='Playlist Name'
+                            className='list-card'
+                            onKeyPress={handleKeyPress}
+                            onBlur={handleBlur}
+                            onChange={handleUpdateText}
+                            defaultValue={idNamePair.name}
+                            inputProps={{style: {fontSize: 48}}}
+                            InputLabelProps={{style: {fontSize: 24}}}
+                            autoFocus
+                        />
+                    );
+                }
+                return cardElement;
+            }
+
+            export default ListCard;
+        }
     }
-    return (
-        cardElement
-    );
 }
-
-export default ListCard;
